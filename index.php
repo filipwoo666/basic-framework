9e51aba1eef566c501c160b77dd8919f8ca908ac
trrtr <filip.wozniak@yahoo.pl>
2016-09-19 19:34:10 +0200

    Removed some lines.

--- index.php
+++ index.php
@@ -12,28 +12,4 @@ $input = $request->get('name', 'World');
 
 $response = new Response(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
 
-// the URI being requested (e.g. /about) minus any query parameters
-//var_dump($request->getPathInfo());
-
-// retrieve GET and POST variables respectively
-var_dump($request->getClientIp());
-$request->query->get('foo');
-$request->request->get('bar', 'default value if bar does not exist');
-
-// retrieve SERVER variables
-$request->server->get('HTTP_HOST');
-
-// retrieves an instance of UploadedFile identified by foo
-$request->files->get('foo');
-
-// retrieve a COOKIE value
-$request->cookies->get('PHPSESSID');
-
-// retrieve an HTTP request header, with normalized, lowercase keys
-$request->headers->get('host');
-$request->headers->get('content_type');
-
-$request->getMethod();    // GET, POST, PUT, DELETE, HEAD
-$request->getLanguages(); // an array of languages the client accepts
-
 $response->send();
